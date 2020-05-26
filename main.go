e618c4e92de0062066eabe3467224cdba7470e98
Janusz Piechówka <januszpiechowka@gmail.com>
2020-05-27 01:40:31 +0200

    Implement printing banner and greeting messages to user

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	printBanner()
+	printGreeting()
+	//repl.Start(os.Stdin, os.Stdout)
+}
+
+func printBanner() {
+	banner := `
+   __  ____                 
+  /  |/  (_)__________  ___ 
+ / /|_/ / / __/ __/ _ \/ _ \
+/_/  /_/_/\__/_/  \___/_//_/
+
+`
+
+	fmt.Print(banner)
+}
+
+func printGreeting() {
+	currentUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! Welcome to the Micron language console!\n", currentUser.Username)
+	fmt.Printf("Feel free to type in the code below\n\n")
 }
