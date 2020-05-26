c4d11d06458161191c76d31cc3719333ee37a186
Janusz Piechówka <januszpiechowka@gmail.com>
2020-05-27 01:12:23 +0200

    Extend lexer input test cases

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,19 +6,55 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `
+		let one = 1;
+		let seven = 7;
+
+		let add = fn(x, y) {
+			x + y;
+		};
+
+		let result = add(one, seven);`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "one"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "1"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "seven"},
+		{token.ASSIGN, "="},
+		{token.INT, "7"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "one"},
 		{token.COMMA, ","},
+		{token.IDENT, "seven"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
