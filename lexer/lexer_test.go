6c67798a75e82d3d2e8a0492cba3d11ad3d2dd87
Janusz Piechówka <januszpiechowka@gmail.com>
2020-05-26 16:11:01 +0200

    Add basic lexer test cases in lexer_test.go file

---
+++ lexer/lexer_test.go
@@ -0,0 +1,39 @@
+package lexer
+
+import (
+	"github.com/jpiechowka/micron-language-interpreter-go/token"
+	"testing"
+)
+
+func TestNExtToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	lexer := New(input)
+
+	for i, tokenType := range tests {
+		testedToken := lexer.NextToken()
+
+		if testedToken.TokenType != tokenType.expectedType {
+			t.Fatalf("lexer test case [%d/%d] TokenType is wrong. Expected %q, got %q", i, len(tests), tokenType.expectedType, testedToken.TokenType)
+		}
+
+		if testedToken.Literal != tokenType.expectedLiteral {
+			t.Fatalf("lexer tests case [%d/%d] Literal is wrong. Expected %q, got %q", i, len(tests), tokenType.expectedLiteral, testedToken.Literal)
+		}
+	}
+}
