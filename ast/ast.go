d33b28c8e6b6cd857bb85f44cc45963c0eb4c18f
Janusz Piechówka <januszpiechowka@gmail.com>
2020-05-30 23:43:02 +0200

    Add initial AST node implementation

--- ast/ast.go
+++ ast/ast.go
@@ -13,3 +13,15 @@ type Expression interface {
 	AstNode
 	expressionNode()
 }
+
+type Program struct {
+	Statements []Statement
+}
+
+func (program *Program) TokenLiteral() string {
+	if len(program.Statements) > 0 {
+		return program.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
