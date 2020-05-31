3fdbe9a9a41b84ed3637fdcba84541bf64815af5
Janusz Piechówka <januszpiechowka@gmail.com>
2020-05-31 18:02:43 +0200

    Add basic parser implementation

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,29 @@
 package parser
+
+import (
+	"github.com/jpiechowka/micron-language-interpreter-go/ast"
+	"github.com/jpiechowka/micron-language-interpreter-go/lexer"
+	"github.com/jpiechowka/micron-language-interpreter-go/token"
+)
+
+type Parser struct {
+	lexer        *lexer.Lexer
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(lexer *lexer.Lexer) *Parser {
+	parser := &Parser{lexer: lexer}
+	parser.nextToken() // Read two tokens so currentToken and peekToken are populated
+	parser.nextToken()
+	return parser
+}
+
+func (parser *Parser) nextToken() {
+	parser.currentToken = parser.peekToken
+	parser.peekToken = parser.lexer.NextToken()
+}
+
+func (parser *Parser) ParseProgram() *ast.Program {
+	return nil
+}
