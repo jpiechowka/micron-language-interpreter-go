5fb2b8656111f386ea9028b69c6e4be808267610
Janusz Piechówka <januszpiechowka@gmail.com>
2020-05-31 22:52:45 +0200

    First implementation of ParseProgram() and basic implementation of parsing let statements

--- parser/parser.go
+++ parser/parser.go
@@ -25,5 +25,64 @@ func (parser *Parser) nextToken() {
 }
 
 func (parser *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{Statements: []ast.Statement{}}
+
+	for parser.currentToken.TokenType != token.EOF {
+		statement := parser.parseStatement()
+
+		if statement != nil {
+			program.Statements = append(program.Statements, statement)
+		}
+
+		parser.nextToken()
+	}
+
+	return program
+}
+
+func (parser *Parser) parseStatement() ast.Statement {
+	switch parser.currentToken.TokenType {
+	case token.LET:
+		return parser.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (parser *Parser) parseLetStatement() *ast.LetStatement {
+	statement := &ast.LetStatement{Token: parser.currentToken}
+
+	if !parser.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	statement.Name = &ast.Identifier{Token: parser.currentToken, Value: parser.currentToken.Literal}
+
+	if !parser.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we encounter a semicolon
+	for !parser.isComparedTokenSameAsCurrent(token.SEMICOLON) {
+		parser.nextToken()
+	}
+
+	return statement
+}
+
+func (parser *Parser) isComparedTokenSameAsCurrent(tokenToCompare token.TokenType) bool {
+	return parser.currentToken.TokenType == tokenToCompare
+}
+
+func (parser *Parser) isComparedTokenSameAsPeek(tokenToCompare token.TokenType) bool {
+	return parser.peekToken.TokenType == tokenToCompare
+}
+
+func (parser *Parser) expectPeek(expectedToken token.TokenType) bool {
+	if parser.isComparedTokenSameAsPeek(expectedToken) {
+		parser.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
