3bb2ce15e6cdc1f4b4aab911d68616b4734f878c
Janusz Piechówka <januszpiechowka@gmail.com>
2020-06-01 22:01:43 +0200

    Add String() methods

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "github.com/jpiechowka/micron-language-interpreter-go/token"
+import (
+	"bytes"
+	"github.com/jpiechowka/micron-language-interpreter-go/token"
+)
 
 type AstNode interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -44,6 +48,7 @@ type Identifier struct {
 
 func (identifier *Identifier) expressionNode()      {}
 func (identifier *Identifier) TokenLiteral() string { return identifier.Token.Literal }
+func (identifier *Identifier) String() string       { return identifier.Value }
 
 type ReturnStatement struct {
 	Token       token.Token
@@ -52,3 +57,61 @@ type ReturnStatement struct {
 
 func (returnStatement *ReturnStatement) statementNode()       {}
 func (returnStatement *ReturnStatement) TokenLiteral() string { return returnStatement.Token.Literal }
+
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (expressionStatement *ExpressionStatement) statementNode() {}
+func (expressionStatement *ExpressionStatement) TokenLiteral() string {
+	return expressionStatement.Token.Literal
+}
+
+func (program *Program) String() string {
+	var out bytes.Buffer
+
+	for _, statement := range program.Statements {
+		out.WriteString(statement.String())
+	}
+
+	return out.String()
+}
+
+func (letStatement *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(letStatement.TokenLiteral() + " ")
+	out.WriteString(letStatement.Name.String())
+	out.WriteString(" = ")
+
+	if letStatement.Value != nil {
+		out.WriteString(letStatement.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (returnStatement *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(returnStatement.TokenLiteral() + " ")
+
+	if returnStatement.ReturnValue != nil {
+		out.WriteString(returnStatement.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (expressionStatement *ExpressionStatement) String() string {
+	if expressionStatement.Expression != nil {
+		return expressionStatement.Expression.String()
+	}
+
+	return ""
+}
