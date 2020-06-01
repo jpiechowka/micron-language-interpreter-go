2ce44279c4e5508d0ad02eda7ee9c3125e99d846
Janusz Piechówka <januszpiechowka@gmail.com>
2020-06-01 22:13:47 +0200

    Add AST test case for string methods

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -1,1 +1,22 @@
 package ast
+
+import (
+	"github.com/jpiechowka/micron-language-interpreter-go/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{TokenType: token.LET, Literal: "let"},
+				Name:  &Identifier{Token: token.Token{TokenType: token.IDENT, Literal: "someVar"}, Value: "someVar"},
+				Value: &Identifier{Token: token.Token{TokenType: token.IDENT, Literal: "anotherVar"}, Value: "anotherVar"},
+			},
+		},
+	}
+
+	if program.String() != "let someVar = anotherVar;" {
+		t.Errorf("AST test string failed - program.String() returned wrong values. Got %s", program.String())
+	}
+}
