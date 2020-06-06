e1111c95493e52a989a7e2b44617391b09c9c786
Janusz Piechówka <januszpiechowka@gmail.com>
2020-06-07 01:53:26 +0200

    Add InfixExpression struct and methods

--- ast/ast.go
+++ ast/ast.go
@@ -96,6 +96,25 @@ func (prefixExpression *PrefixExpression) String() string {
 	return out.String()
 }
 
+type InfixExpression struct {
+	Token    token.Token // The operator token, e.g. +
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (infixExpression *InfixExpression) expressionNode()      {}
+func (infixExpression *InfixExpression) TokenLiteral() string { return infixExpression.Token.Literal }
+func (infixExpression *InfixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(infixExpression.Left.String())
+	out.WriteString(" " + infixExpression.Operator + " ")
+	out.WriteString(infixExpression.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
+
 func (program *Program) String() string {
 	var out bytes.Buffer
 
